109206d1a9f3f6354814cdcc4c90172a3032340e
Nicolas Mémeint <nicomem@pm.me>
2021-01-19 12:14:34 +0100

    Add slice sequence with configurable batch_size

--- notebooks/utils/load_data.py
+++ notebooks/utils/load_data.py
@@ -4,6 +4,83 @@ from tensorflow.keras.utils import Sequence
 
 from pathlib import Path
 
+class SlicesSequence(Sequence):
+    '''
+    Helper class for slices lazy loading.
+    Each index access correspond to a batch of scan slices.
+    '''
+
+    @staticmethod
+    def create_indexes(dataset_nib: dict, shuffle: bool) -> np.ndarray:
+        # Get the number of slices for each scan
+        scans_len = [scan.shape[-1] for scan in dataset_nib['wmh']]
+
+        # Create the (i_scan, i_slices) array
+        ranges = [
+            [i_scan, i_slice]
+            for i_scan in range(len(scans_len))
+            for i_slice in range(scans_len[i_scan])
+        ]
+        indexes = np.asarray(ranges, dtype=np.uint16)
+
+        # Shuffle the indexes
+        np.random.shuffle(indexes)
+        return indexes
+
+    def __init__(self, dataset_nib: dict, batch_size: int, shuffle = True):
+        self.dataset_nib = dataset_nib
+        self.shuffle = shuffle
+        self.batch_size = batch_size
+        self.indexes = SlicesSequence.create_indexes(dataset_nib, shuffle)
+
+    def __len__(self):
+        # Number of batches: floor(nb_samples / batch_size)
+        return len(self.indexes) // self.batch_size
+
+    def __getitem__(self, idx: int) -> (np.ndarray, np.ndarray):
+        # Check for index out of range
+        len_self = len(self)
+        if idx >= len_self or idx < -len_self:
+            raise IndexError('Sequence index out of range')
+
+        # Allow negative index like python list
+        if idx < 0:
+            idx += len_self
+
+        # Get the current batch
+        indexes = self.indexes[idx*self.batch_size:(idx+1)*self.batch_size]
+
+        # Do not use '3DT1' since its slices shapes are weird
+        columns = ['T1', 'FLAIR', 'wmh']
+
+        # Load all slices for the wanted element
+        batch_dic = {
+            k: np.asarray([
+                # Load the wanted slice from index=[i_scan, i_slice]
+                np.asarray(self.dataset_nib[k][index[0]].dataobj[...,index[1]])
+                for index in indexes
+            ], dtype=np.ndarray)
+            for k in columns
+        }
+
+        # TODO: Reshape the slices
+
+        # Regroup all inputs together
+        inputs = np.stack([
+            batch_dic['T1'],
+            batch_dic['FLAIR']
+        ], axis=-1)
+
+        # Add the channels axis (single channel)
+        outputs = batch_dic['wmh']
+
+        return inputs, outputs
+
+    def on_epoch_end(self):
+        # Shuffle the indexes
+        np.random.shuffle(self.indexes)
+
+
 class NibDataSequence(Sequence):
     '''
     Helper class for dataset lazy loading.
@@ -66,7 +143,7 @@ class CachedDataSequence(Sequence):
         return self.X[idx], self.Y[idx]
 
 
-def get_dataset(data_dir: str, train_ratio = 0.9, verbose = False):
+def get_dataset(data_dir: str, train_ratio = 0.9, verbose = False) -> (dict, dict):
     """
     Load the dataset and split it into train/validation sets.
 
@@ -135,3 +212,26 @@ def get_dataset(data_dir: str, train_ratio = 0.9, verbose = False):
     }
 
     return (train_nib, val_nib)
+
+from timeit import timeit
+if __name__ == '__main__':
+    train, val = get_dataset('../../data')
+
+    slices = SlicesSequence(train, 32, shuffle=False)
+    print(slices[0])
+
+    print('Ordered')
+    slices = SlicesSequence(train, 32, shuffle=False)
+    print(timeit(lambda: slices[0], number=5))
+
+    print('----------')
+
+    print('Shuffled')
+    slices = SlicesSequence(train, 32, shuffle=True)
+    print(timeit(lambda: slices[0], number=5))
+
+    # i = 0
+    # while True:
+    #     x,y = slices[i % len(slices)]
+    #     print(i, x.shape, y.shape)
+    #     i += 1
